8b5e6a116cdc4d2bb01ffa9c9a029b6193edc979
wojciechmituta <38225947+wojciechmituta@users.noreply.github.com>
2019-05-22 20:08:29 +0100

    next excercises

---
+++ src/test/java/codeWars/kyu8/EvenNumberTest.java
@@ -0,0 +1,16 @@
+package codeWars.kyu8;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertTrue;
+
+public class EvenNumberTest {
+    @Test
+    public void testSimple() {
+        assertTrue(Arrays.equals(new int[]{2, 4, 6}, EvenNumbers.divisibleBy(new int[]{1, 2, 3, 4, 5, 6}, 2)));
+        assertTrue(Arrays.equals(new int[]{3, 6}, EvenNumbers.divisibleBy(new int[]{1, 2, 3, 4, 5, 6}, 3)));
+        assertTrue(Arrays.equals(new int[]{0, 4}, EvenNumbers.divisibleBy(new int[]{0, 1, 2, 3, 4, 5, 6}, 4)));
+    }
+}
